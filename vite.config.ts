5b434de422740ae1e38a6c65c7ca54031c759381
kimmeh <mhko0411@gmail.com>
2025-05-01 18:01:13 +0900

    fix: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,32 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
+      outDir: 'dist',
+      include: ['src'],
     }),
   ],
   build: {
+    target: 'esnext',
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "npm-package-template",
-      formats: ["es", "cjs"],
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'file-embedra',
+      formats: ['es', 'cjs'],
       fileName: (format) => {
-        if (format === "es") {
-          return "index.js";
+        if (format === 'es') {
+          return 'index.js';
         }
 
-        return "index.cjs";
+        return 'index.cjs';
       },
+    },
+    rollupOptions: {
+      external: ['openai', 'fs'],
     },
   },
 });
